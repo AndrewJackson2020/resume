222c9cc8fe7248ca75e1dc30522caa6745368b9d
CommanderKeynes <andrewjackson947@gmail.com>
2023-07-24 10:33:43 -0500

    Wrote CLI to wrap configuration

--- cli.sh
+++ cli.sh
@@ -26,23 +26,60 @@ build_resume_docker () {
 
 build_resume () {
 
-	pdflatex \
-		-output-dir "output" \
-		"\\def\\whatever{financial}  \\input{resume.tex}"
+	resume_type=software
+	
+	for i in "$@"
+	do
+		case $i in
+			-t=*|--resume_type=*)
+				resume_type="${i#*=}"
+				;;
+			-p=*|--phone=*)
+				phone_number="${i#*=}"
+				;;
+			-c=*|--city=*)
+				city="${i#*=}"
+				;;
+			-z=*|--z=*)
+				zip_code="${i#*=}"
+				;;
+			-*|--*)
+				exit 1
+				;;
+			*)
+				exit 1
+				;;
+		esac
+	done
 
+	mkdir --parents ./temp
+	rm ./variables.tex
+	touch ./variables.tex
+	cat << EOF > ./variables.tex
+\\newcommand{\\resumeType}{${resume_type}}
+\\newcommand{\\phoneVar}{${phone_number}}
+\\newcommand{\\homeTownVar}{${city}}
+\\newcommand{\\homeZipCodeVar}{${zip_code}}
+EOF
+	pdflatex \
+		-output-dir "temp" \
+		./resume.tex
+
+	mv ./temp/resume.pdf ./
+	rm -r ./temp
+	rm ./variables.tex
 }
 
 
 cli () {
 	case $1 in
 
-		--build_resume | -b)    
-			build_resume 			
-			# build_resume_pdf 
+		build_resume)    
+			build_resume "${@:2}"
 			;;
 
-		--build_resume_docker | -bd)
-			build_resume_docker 
+		build_resume_docker)
+			build_resume_docker "$@"
 			;;
 
 		--help | -h)

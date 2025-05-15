e1935846510bd9159116ce5808ee8fb501e1db41
CommanderKeynes <andrewjackson947@gmail.coma>
2025-05-14 22:08:12 -0500

    Add nvidia drivers

--- nix-os-config/hardware_configuration/desktop.nix
+++ nix-os-config/hardware_configuration/desktop.nix
@@ -43,6 +43,10 @@
 
   swapDevices = [ ];
 
+
+    services.xserver.videoDrivers = [ "nvidia" ];
+    hardware.nvidia.open=false;
+
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking (the default) this is the recommended approach. When using 
   # systemd-networkd it's still possible to use this option, but it's recommended to use it in conjunction with explicit per-interface declarations 
   # with `networking.interfaces.<interface>.useDHCP`.

7c49cd23bf98a5eef666a9c163850801ababaa9e
CommanderKeynes <andrewjackson947@gmail.coma>
2025-05-15 21:07:59 -0500

    Add samba share

--- nix-os-config/hardware_configuration/desktop.nix
+++ nix-os-config/hardware_configuration/desktop.nix
@@ -45,7 +45,44 @@
     };
 
   swapDevices = [ ];
+services.samba = {
+  enable = true;
+  securityType = "user";
+  openFirewall = true;
+  settings = {
+    global = {
+      "workgroup" = "WORKGROUP";
+      "server string" = "smbnix";
+      "netbios name" = "smbnix";
+      "security" = "user";
+      #"use sendfile" = "yes";
+      #"max protocol" = "smb2";
+      # note: localhost is the ipv6 localhost ::1
+      "hosts allow" = "0.0.0.0/0 192.168.0. 127.0.0.1 localhost";
+      # "hosts deny" = "0.0.0.0/0";
+      "guest account" = "nobody";
+      "map to guest" = "bad user";
+    };
+    "public" = {
+      "path" = "/mnt/backup_data";
+      "browseable" = "yes";
+      "read only" = "no";
+      "guest ok" = "yes";
+      "create mask" = "0644";
+      "directory mask" = "0755";
+      "force user" = "username";
+      "force group" = "groupname";
+    };
+  };
+};
 
+services.samba-wsdd = {
+  enable = true;
+  openFirewall = true;
+};
+
+networking.firewall.enable = true;
+networking.firewall.allowPing = true;
 
     services.xserver.videoDrivers = [ "nvidia" ];
     hardware.nvidia.open=false;
